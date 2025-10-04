c7bfe6cd700a1316f7ffda025d4469a66651d7e5
g1mberg <139089110+g1mberg@users.noreply.github.com>
2025-10-04 20:52:36 +0300

    hw2

---
+++ HWs/HttpServer/HttpServer/Shared/SettingsModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace HttpServer.Shared;
+
+public class SettingsModel
+{
+    public string StaticDirectoryPath { get; set; }
+    public string Domain { get; set; }
+    public string Port { get; set; }
+
+    public static SettingsModel ReadJSON(string path) =>
+        JsonSerializer.Deserialize<SettingsModel>(File.ReadAllText(path));
+}
